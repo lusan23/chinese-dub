ec84cfbb73d1889c4d773760bf1d0d3f93070e6f
lusan23 <luanlol450@gmail.com>
2023-08-17 06:04:21 -0300

    Video class created

---
+++ main_page.py
@@ -0,0 +1,16 @@
+"""
+Get the youtube video and its subtitles.
+"""
+from python import Youtube
+class Video():
+    """
+    Class representing the video.
+    """
+
+def __init__(self):
+    """
+    Create attributes to extract from the video
+    """
+    self.url = ""
+    self.subtitles = ""
+    self.video_file = ""
